853676c23539db0f54d09b52016a11aa8be0a3bb
Manu Raj U A <manuraj2402@gmail.com>
2024-12-24 12:55:41 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,22 @@
+//create web server
+//create a file called comments.js
+//create a server object
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+http.createServer(function (req, res) {
+  //parse the url
+  var q = url.parse(req.url, true);
+  var filename = "." + q.pathname;
+  //read the file
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    }  
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
